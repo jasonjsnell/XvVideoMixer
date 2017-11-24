eb42ee0328c0d8625aad68caea043242b7d8d24a
jasonjsnell <snell.jason@gmail>
2017-11-24 10:00:05 -0800

    Initial Commit

---
+++ XvVideoMixer/XvVideoMixer.h
@@ -0,0 +1,19 @@
+//
+//  XvVideoMixer.h
+//  XvVideoMixer
+//
+//  Created by Jason Snell on 11/24/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvVideoMixer.
+FOUNDATION_EXPORT double XvVideoMixerVersionNumber;
+
+//! Project version string for XvVideoMixer.
+FOUNDATION_EXPORT const unsigned char XvVideoMixerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvVideoMixer/PublicHeader.h>
+
+
